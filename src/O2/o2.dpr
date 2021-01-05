2447e6f552912d57115181f8ebc6b55225c8a1ef
maurizuki <62178459+maurizuki@users.noreply.github.com>
2021-01-05 14:50:35 +0100

    Update copyright

--- src/O2/o2.dpr
+++ src/O2/o2.dpr
@@ -7,7 +7,7 @@
 { The initial Contributor is Maurizio Basaglia.                        }
 {                                                                      }
 { Portions created by the initial Contributor are Copyright (C)        }
-{ 2004-2015 the initial Contributor. All rights reserved.              }
+{ 2004-2021 the initial Contributor. All rights reserved.              }
 {                                                                      }
 { Contributor(s):                                                      }
 {                                                                      }
