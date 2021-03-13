b243c01d64ac566072601b3ce91dfa0a243ad19c
maurizuki <62178459+maurizuki@users.noreply.github.com>
2021-03-13 16:21:09 +0100

    Use ● instead of * as password char

--- src/Library/uO2Rules.pas
+++ src/Library/uO2Rules.pas
@@ -1,4 +1,4 @@
-{ ******************************************************************** }
+﻿{ ******************************************************************** }
 {                                                                      }
 { This Source Code Form is subject to the terms of the Mozilla Public  }
 { License, v. 2.0. If a copy of the MPL was not distributed with this  }
@@ -21,7 +21,7 @@ uses
   Classes, Windows, SysUtils, Graphics, uO2Classes, uO2Objects;
 
 const
-  PasswordChar = #42;
+  PasswordChar = '●';
 
 { Rule param names }
 
