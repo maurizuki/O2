96e569dc94fd95cd85c00c46a3613d27cba86dcf
Maurizio Basaglia <62178459+maurizuki@users.noreply.github.com>
2021-05-19 19:13:43 +0200

    Block deprecated hash algorithms (#1)
    * Default hash algorithm SHA-1 -> SHA-256
    
    * Display deprecated hash algorithms
    
    * Force to change hash algorithm if deprecated

--- src/Library/uO2Defs.pas
+++ src/Library/uO2Defs.pas
@@ -76,7 +76,9 @@ const
   ohSHA384    = TO2Hash($08);
   ohSHA512    = TO2Hash($09);
   ohTiger     = TO2Hash($0A);
-  ohDefault   = ohSHA1;
+  ohDefault   = ohSHA256;
+
+  DeprecatedHashes: set of TO2Hash = [ohMD5, ohSHA1];
 
 procedure GetCipherValues(Proc: TGetStrProc);
 function CipherToIdent(Cipher: Longint; var Ident: string): Boolean;
