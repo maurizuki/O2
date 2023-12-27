375de12c09575fe3e59f5d7b47f67900d4516207
maurizuki <62178459+maurizuki@users.noreply.github.com>
2023-12-27 18:11:00 +0100

    Update uO2Defs.pas

--- src/Library/uO2Defs.pas
+++ src/Library/uO2Defs.pas
@@ -22,7 +22,7 @@ uses
 
 const
   O2FileGUID: TGUID = '{ABBB4FE2-9C21-450E-80B0-469DFD8A8BFC}';
-  O2FileVersion: WordRec = (Lo: 2; Hi: 2); // 2.2
+  O2FileVersion: WordRec = (Lo: 3; Hi: 2); // 2.3
 
 type
   TO2Cipher = Byte;
