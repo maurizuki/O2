cb902fa53234c7ecbefa0d58b8d9c4c2a19e279d
maurizuki <62178459+maurizuki@users.noreply.github.com>
2024-03-01 12:13:40 +0100

    Update uServices.pas

--- src/O2/uServices.pas
+++ src/O2/uServices.pas
@@ -33,6 +33,7 @@ type
   IStorage = interface
     function Exists(const Name: string): Boolean;
     procedure Delete(const Name: string);
+
     function ReadBoolean(const Name: string; Default: Boolean = False): Boolean;
     function ReadInteger(const Name: string; Default: Integer = 0): Integer;
     function ReadIntIdent(const Name: string;
@@ -40,6 +41,7 @@ type
     function ReadFloat(const Name: string; Default: Double = 0): Double;
     function ReadString(const Name: string; Default: string = ''): string;
     procedure ReadStringList(const Name: string; const AStringList: TStrings);
+
     procedure WriteBoolean(const Name: string; Value: Boolean);
     procedure WriteInteger(const Name: string; Value: Integer);
     procedure WriteIntIdent(const Name: string;
@@ -47,6 +49,7 @@ type
     procedure WriteFloat(const Name: string; Value: Double);
     procedure WriteString(const Name, Value: string);
     procedure WriteStringList(const Name: string; const AStringList: TStrings);
+
     procedure LoadFromFile(const FileName: string);
     procedure SaveToFile(const FileName: string);
   end;
